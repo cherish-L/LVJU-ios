d461492d5f65c849ed9143b932ab93b9d041878a
ljc-develop <15632877402@163.com>
2017-08-03 10:29:38 +0800

    20170803-1

--- js/list-details.js
+++ js/list-details.js
@@ -27,46 +27,46 @@ $(function(){
 	
 	
 	
-	//楼盘头部滚动监听事件
-	var sectionS = new IScroll('.section', {
-		scrollbars: false
-	});
-	
-	//弹框头部滚动监听事件
-	var pushS = new IScroll('.list-details-pushsection', {
-		scrollbars: false,
-		probeType: 3
-	});
-	var houseStyleS = new IScroll('.houseStyle-content', {
-		scrollbars: false,
-		scrollX: true,
-		scrollY: false
-	});
-	
-	pushS.on('scroll',function(){
-		if(pushS.y < 0){
-			var Scrolly=-(pushS.y/180)
-			$(".list-details-pushheader").css("background-color","rgba(246, 246, 246,"+Scrolly+")")
-			$(".list-details-pushheader").css("box-shadow","0 1px 0 0 rgba(204,204,204,"+Scrolly+")")
-			$(".title_txt").css("color","rgba(57, 64, 67,"+Scrolly+")")
-			if(pushS.y < -160){
-				$(".return").addClass("state")
-				$(".collection").addClass("state")
-				$(".share").addClass("state")
-			}
-			if(pushS.y >= -160){
-				$(".return").removeClass("state")
-				$(".collection").removeClass("state")
-				$(".share").removeClass("state")
-			}
-		}
-		if(pushS.y >= 0){
-			$(".list-details-pushheader").css("background-color","rgba(246, 246, 246,0.0)")
-			$(".list-details-pushheader").css("box-shadow","0 1px 0 0 rgba(204,204,204,0.0)")
-			$(".title_txt").css("color","rgba(57, 64, 67,0.0)")
-		}
-		
-	});
+//	//楼盘头部滚动监听事件
+//	var sectionS = new IScroll('.section', {
+//		scrollbars: false
+//	});
+//	
+//	//弹框头部滚动监听事件
+//	var pushS = new IScroll('.list-details-pushsection', {
+//		scrollbars: false,
+//		probeType: 3
+//	});
+//	var houseStyleS = new IScroll('.houseStyle-content', {
+//		scrollbars: false,
+//		scrollX: true,
+//		scrollY: false
+//	});
+//	
+//	pushS.on('scroll',function(){
+//		if(pushS.y < 0){
+//			var Scrolly=-(pushS.y/180)
+//			$(".list-details-pushheader").css("background-color","rgba(246, 246, 246,"+Scrolly+")")
+//			$(".list-details-pushheader").css("box-shadow","0 1px 0 0 rgba(204,204,204,"+Scrolly+")")
+//			$(".title_txt").css("color","rgba(57, 64, 67,"+Scrolly+")")
+//			if(pushS.y < -160){
+//				$(".return").addClass("state")
+//				$(".collection").addClass("state")
+//				$(".share").addClass("state")
+//			}
+//			if(pushS.y >= -160){
+//				$(".return").removeClass("state")
+//				$(".collection").removeClass("state")
+//				$(".share").removeClass("state")
+//			}
+//		}
+//		if(pushS.y >= 0){
+//			$(".list-details-pushheader").css("background-color","rgba(246, 246, 246,0.0)")
+//			$(".list-details-pushheader").css("box-shadow","0 1px 0 0 rgba(204,204,204,0.0)")
+//			$(".title_txt").css("color","rgba(57, 64, 67,0.0)")
+//		}
+//		
+//	});
 	
 	
 })
