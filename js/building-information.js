c411c6b31b86200efd8916ee1ba7ed6e3990efdb
ljc-develop <15632877402@163.com>
2017-08-04 14:21:24 +0800

    20170804

--- js/building-information.js
+++ js/building-information.js
@@ -1,70 +1,71 @@
-$(function(){
+$(function() {
 	//定义navsub-ul 的宽度
-	var liwidth=$(".navsub-ul li").width()
-	var lilen=$(".navsub-ul li").length
-	$(".navsub-ul").width(liwidth*lilen)
+	var liwidth = $(".navsub-ul li").width()
+	var lilen = $(".navsub-ul li").length
+	$(".navsub-ul").width(liwidth * lilen)
 	//navsub-ul点击效果
-	$(".navsub-ul li").tap(function(){
+	$(".navsub-ul li").tap(function() {
 		$(this).addClass("navsub-ul-active").siblings().removeClass("navsub-ul-active")
 	})
-	
+
 	//article-push
-	var metaw=$(".section-content-body").width()
-	$(".article-push").css("left",metaw)
-	$(".section-content-massage .pri-li").tap(function(){
-		$(".article-push").css("left","0")
+	var metaw = $(".section-content-body").width()
+	$(".article-push").css("left", metaw)
+	$(".section-content-massage .pri-li").tap(function() {
+		$(".article-push").css("left", "0")
 		atPushS.refresh()
 	})
-	
-	$(".article-push-header-wrapper .return").tap(function(){
-		$(".article-push").css("left",metaw)
+
+	$(".article-push-header-wrapper .return").tap(function() {
+		$(".article-push").css("left", metaw)
 		atPushS.refresh()
 	})
-	
-	
+
 	//input文本框
-	var panelw=$(".writing-panel").height()
-	$(".article-push-footer .write-reviews").tap(function(){
-		$(".zhezhao").css("z-index","50")
-		$(".zhezhao").css("opacity","1")
-		$(".writing-box").css("bottom","0")
+	var panelw = $(".writing-panel").height()
+	$(".article-push-footer .write-reviews").tap(function() {
+		$(".zhezhao").css("z-index", "50")
+		$(".zhezhao").css("opacity", "1")
+		$(".writing-box").css({
+			"position": "static",
+			"bottom": 0
+		});
 		atPushS.refresh()
-		
+
 	})
-	$('.zhezhao').bind("touchstart",function(event){
-		$(".zhezhao").css("z-index","-1")
-		$(".zhezhao").css("opacity","0")
-		$(".writing-box").css("bottom",-panelw)
+
+//	$('.writing-box').bind("focus", function() {
+//		
+//	}).bind("blur", function() {
+//		$(".div_ft").css("position", "fixed");
+//	});
+
+	$('.zhezhao').bind("touchstart", function(event) {
+		$(".zhezhao").css("z-index", "-1")
+		$(".zhezhao").css("opacity", "0")
+		$(".writing-box").css("bottom", -panelw)
 		atPushS.refresh()
-  });
-	$(".cancel").tap(function(){
-		$(".zhezhao").css("z-index","-1")
-		$(".zhezhao").css("opacity","0")
-		$(".writing-box").css("bottom",-panelw)
+	});
+	$(".cancel").tap(function() {
+		$(".zhezhao").css("z-index", "-1")
+		$(".zhezhao").css("opacity", "0")
+		$(".writing-box").css("bottom", -panelw)
 		atPushS.refresh()
 	})
-	$(".send").tap(function(){
-		$(".zhezhao").css("z-index","-1")
-		$(".zhezhao").css("opacity","0")
-		$(".writing-box").css("bottom",-panelw)
+	$(".send").tap(function() {
+		$(".zhezhao").css("z-index", "-1")
+		$(".zhezhao").css("opacity", "0")
+		$(".writing-box").css("bottom", -panelw)
 		$(".text-box").val("")
 		atPushS.refresh()
 	})
-	
-	
-	$(".showall-wrapper").tap(function(){
+
+	$(".showall-wrapper").tap(function() {
 		$(".user-say").removeClass("showmore")
-		$(".showall").css("display","none")
+		$(".showall").css("display", "none")
 		atPushS.refresh()
 	})
-	
-	
-	
-	
-	
-	
-	
-	
+
 	var navS = new IScroll('.content-nav', {
 		scrollbars: false,
 		scrollX: true,
@@ -76,6 +77,5 @@ $(function(){
 	var atPushS = new IScroll('.article-push-body', {
 		scrollbars: false
 	});
-	
-	
+
 })
