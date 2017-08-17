aa9636c7c0543945edced9c044e03286312a4cf3
ljc-develop <15632877402@163.com>
2017-08-17 16:03:21 +0800

    1234567890

--- js/list-details.js
+++ js/list-details.js
@@ -974,25 +974,25 @@ $(function() {
 	var PropertyinfoS = new IScroll('.Property-info-pushsection', {
 		scrollbars: false
 	})
-//	var discountS = new IScroll('.firstPage', {
-//		scrollbars: false
-//	})
-//	var discountS = new IScroll('.secondPage', {
-//		scrollbars: false
-//	})
-//	var discountS = new IScroll('.thirdPage', {
-//		scrollbars: false
-//	})
-//	var discountS = new IScroll('.fourthPage', {
-//		scrollbars: false
-//	})
-//	var discountS = new IScroll('.fifthPage', {
-//		scrollbars: false
-//	})
-//	var discountS = new IScroll('.sixthPage', {
-//		scrollbars: false
-//	})
-//	var discountS = new IScroll('.seventhPage', {
-//		scrollbars: false
-//	})
+	var firstPageS = new IScroll('.firstPage', {
+		scrollbars: false
+	})
+	var secondPageS = new IScroll('.secondPage', {
+		scrollbars: false
+	})
+	var thirdPageS = new IScroll('.thirdPage', {
+		scrollbars: false
+	})
+	var fourthPageS = new IScroll('.fourthPage', {
+		scrollbars: false
+	})
+	var fifthPageS = new IScroll('.fifthPage', {
+		scrollbars: false
+	})
+	var sixthPageS = new IScroll('.sixthPage', {
+		scrollbars: false
+	})
+	var seventhPageS = new IScroll('.seventhPage', {
+		scrollbars: false
+	})
 })
