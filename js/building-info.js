d4530b70bfeeba8af11f4d3e33a2c1ac33a819af
ljc-develop <15632877402@163.com>
2017-09-09 15:53:31 +0800

    1234

--- js/building-info.js
+++ js/building-info.js
@@ -3,11 +3,8 @@ $(function(){
 		building_infoS.refresh()
 	})
 	var building_infoS = new IScroll('.building-info-section', {
-		scrollbars: true,
-		fadeScrollbars: true,
-		shrinkScrollbars: 'clip'
+		scrollbars: false
 	})
-	
 	
 	var section_w=$(".building-info-section").width()
 	$(".building-info-banner").height(section_w*1.1)
