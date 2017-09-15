270693898282fd2411b4c23e44d69eadb46f79ea
ljc-develop <15632877402@163.com>
2017-09-15 10:33:47 +0800

    11

--- js/album-page.js
+++ js/album-page.js
@@ -3,10 +3,10 @@ $(function() {
 	var meta_w = $("html").width()
 	var meta_h = $("html").height()
 	if(meta_h > meta_w) {
-		var album_h = $(".album-pushsection-vertical").height()
-		var img_box = $(".album-pushsection-vertical .img-box").height()
-		$(".album-pushsection-vertical .top-txt").height((album_h - img_box) * 0.39)
-		$(".album-pushsection-vertical .bottom-nav").height((album_h - img_box) * 0.61)
+		var album_h = $(".album-pushsection").height()
+		var img_box = $(".album-pushsection .img-box").height()
+		$(".album-pushsection .top-txt").height((album_h - img_box) * 0.39)
+		$(".album-pushsection .bottom-nav").height((album_h - img_box) * 0.61)
 	}
 
 
