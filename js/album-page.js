d16dde6b553f61cdae96b62f9d70d589cf32c91f
ljc-develop <15632877402@163.com>
2017-09-15 15:46:40 +0800

    123456789

--- js/album-page.js
+++ js/album-page.js
@@ -1,13 +1,40 @@
 $(function() {
-
-	var meta_w = $("html").width()
-	var meta_h = $("html").height()
-	if(meta_h > meta_w) {
-		var album_h = $(".album-pushsection").height()
-		var img_box = $(".album-pushsection .img-box").height()
-		$(".album-pushsection .top-txt").height((album_h - img_box) * 0.39)
-		$(".album-pushsection .bottom-nav").height((album_h - img_box) * 0.61)
+	var c=0;
+	var s=0;
+	var li_len=$(".album-page-section .section-wrapper-vertical .bottom-nav-ul li").length
+	var album_h = $(".album-page-section").height()
+	var img_box = $(".album-page-section .section-wrapper-vertical .img-box").height()
+	$(".album-page-section .section-wrapper-vertical .top-txt").height((album_h - img_box) * 0.39)
+	$(".album-page-section .section-wrapper-vertical .img-box").height(album_h-(album_h - img_box) * 0.39)
+	for(var i=0;i<li_len;i++){
+		c=$(".album-page-section .section-wrapper-vertical .bottom-nav-ul li").eq(i).width()
+		s=s+c
 	}
-
-
+	$(".album-page-section .section-wrapper-vertical .bottom-nav-ul").width(s+5)
+	
+	
+	var sale_w = $(".section-wrapper-transverse .sale-name").width()
+	var current_w = $(".section-wrapper-transverse .current-page").width()
+	$(".section-wrapper-transverse .top-txt").width(sale_w+current_w+10)
+	
+	var li_w=0;
+	var li_total_w=0;
+	for(var i=0;i<li_len;i++){
+		li_w=$(".album-page-section .section-wrapper-transverse .bottom-nav-ul li").eq(i).width()
+		li_total_w=li_total_w+li_w
+	}
+	$(".album-page-section .section-wrapper-transverse .bottom-nav-ul").width(li_total_w+5)
+	
+	
+	$(".album-page-section .section-wrapper-vertical .bottom-nav-ul li").tap(function(){
+		var _this_index=$(this).index()
+		$(this).addClass("select").siblings().removeClass("select")
+		$(".album-page-section .section-wrapper-transverse .bottom-nav-ul li").eq(_this_index).addClass("select").siblings().removeClass("select")
+	})
+	
+	$(".album-page-section .section-wrapper-transverse .bottom-nav-ul li").tap(function(){
+		var _this_index=$(this).index()
+		$(this).addClass("select").siblings().removeClass("select")
+		$(".album-page-section .section-wrapper-vertical .bottom-nav-ul li").eq(_this_index).addClass("select").siblings().removeClass("select")
+	})
 })
