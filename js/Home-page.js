27d6e523a4c771f90016dba8ac8106c7fd72d1ef
ljc-develop <15632877402@163.com>
2017-09-18 17:22:06 +0800

    12345

--- js/Home-page.js
+++ js/Home-page.js
@@ -140,4 +140,172 @@ $(function(){
 			$(".city-position").css("left",tcw_ht+5)
 		})
 	})
+	
+	
+//地图找房
+	$(".Home-page-section .section-nav .map_house").tap(function(){
+		$(".map-house").css("left",0)
+		$(".map-house-header .title_bar .return").tap(function(){
+			$(".map-house").css("left",tcw_ht+5)
+		})
+	})
+
+
+	$(".map-house-wrapper .section-map-list .map-list-trand").tap(function(){
+		if($(this).hasClass("click")){
+			$(this).removeClass("click")
+			$(".map-house-wrapper .section-map-list .map-list-tranddata").removeClass("hide")
+		}
+		else{
+			$(this).addClass("click")
+			$(".map-house-wrapper .section-map-list .map-list-tranddata").addClass("hide")
+		}
+	})
+	
+	var map_housew=$(".map-house-wrapper").width()
+	var map_house_index=0;
+	$(".map-house-section").tap(function(){
+		map_house_index++;
+		if(map_house_index%2==1){
+			$(".map-house-wrapper .map-list-btn").addClass("listhide")
+			$(".map-house-wrapper .map-list-trand").addClass("trandhide")
+			$(".map-house-wrapper .map-list-location").addClass("locationhide")
+			$(".map-house-wrapper .map-list-tranddata").addClass("tranddatahide")
+			$(".map-house-wrapper .section-map-peripheral").addClass("peripheralhide")
+		}
+		else{
+			$(".map-house-wrapper .map-list-btn").removeClass("listhide")
+			$(".map-house-wrapper .map-list-trand").removeClass("trandhide")
+			$(".map-house-wrapper .map-list-location").removeClass("locationhide")
+			$(".map-house-wrapper .map-list-tranddata").removeClass("tranddatahide")
+			$(".map-house-wrapper .section-map-peripheral").removeClass("peripheralhide")
+		}
+	})
+	
+	//周边列表
+	$(".map-house-wrapper .section-map-list .map-list-btn .listli").tap(function(){
+		$(".map-house-wrapper .section-map-list").css("left",-0.3*map_housew)
+		$(".map-house-wrapper .section-map-list").css("opacity","0")
+		$(".map-house-wrapper .section-map-peripheral").css("left","0")
+		$(".map-house-wrapper .peripheral-return").css("opacity","1")
+	})
+	$(".map-house-wrapper .section-map-peripheral .peripheral-return").tap(function(){
+		$(".map-house-wrapper .section-map-list").css("left","0")
+		$(".map-house-wrapper .section-map-list").css("opacity","1")
+		$(".map-house-wrapper .section-map-peripheral").css("left",map_housew)
+		$(".map-house-wrapper .peripheral-return").css("opacity","0")
+		
+	})
+	$(".peripheral-li").tap(function(){
+		$(this).addClass("on").siblings().removeClass("on")
+	})
+	
+	//区域弹框和筛选弹框
+	var htmlh=$("html").height()
+	$(".map-house-wrapper .screen-push").css("bottom",-htmlh-5)
+	$(".map-house-wrapper .region-push").css("bottom",-htmlh-5)
+		//筛选弹框
+		$(".map-house-wrapper .screen-push .select").tap(function(){
+			$(this).addClass("selected")
+		})
+		$(".screen-push-footer .empty").tap(function(){
+			$(".map-house-wrapper .screen-push .select").removeClass("selected")
+			$(".section-map-list .map-list-btn .screenli .num").css("opacity","0")
+			screenS.refresh()
+		})
+		$(".screen-push-footer .determine").tap(function(){
+			var len=$(".map-house-wrapper .screen-push .selected").length
+			$(".map-house-wrapper .screen-push").css("bottom",-htmlh-5)
+			$(".section-map-list .map-list-btn .screenli .num").css("opacity","1")
+			$(".section-map-list .map-list-btn .screenli .num span").text(len)
+			if($(".section-map-list .map-list-btn .screenli .num span").text()=="0"){
+				$(".section-map-list .map-list-btn .screenli .num").css("opacity","0")
+			}
+		})
+		$(".screen-push-header .return").tap(function(){
+			$(".map-house-wrapper .screen-push").css("bottom",-htmlh-5)
+		})
+		$(".map-house-wrapper .section-map-list .map-list-btn .screenli").tap(function(){
+			$(".map-house-wrapper .screen-push").css("bottom","0")
+		})
+	
+	
+		//区域弹框
+		var regionp=$(".region-push-section").height()
+		$(".region-push-section-wrapper .regionlist").css("height",regionp)
+		$(".region-push-section-wrapper .municipality").css("height",regionp)
+		$(".region-push-section-wrapper .district").css("height",regionp)
+		
+		$(".region-push-footer .determine").tap(function(){
+			$(".region-push").css("bottom",-htmlh-5)
+		})
+		$(".region-push-header .return").tap(function(){
+			$(".region-push").css("bottom",-htmlh-5)
+		})
+		$(".map-house-wrapper .section-map-list .map-list-btn .regionli").tap(function(){
+			$(".region-push").css("bottom","0")
+		})
+		
+	
+		//区域分栏
+		$(".region-push-section-wrapper .district .district-ul").css("display","none")
+		
+		$(".region-push-section-wrapper .municipality-ul .municipality-li:first-child").addClass("confirmtwo")
+		$(".region-push-section-wrapper .district-ul .district-li:first-child").addClass("confirmthree")
+		$(".region-push-section-wrapper .regionlist-ul .regionlist-li").tap(function(){
+			var index=$(this).index()
+			$(".region-push-section-wrapper .regionlist-ul .regionlist-li").eq(index).addClass("confirmone").siblings().removeClass("confirmone")
+			if(index==1){
+				$(".region-push-section-wrapper .district-ul").css("display","none")
+				$(".region-push-section-wrapper .municipality-ul .municipality-li").removeClass("confirmtwo")
+				$(".region-push-section-wrapper .district-ul .district-li").removeClass("confirmthree")
+				$(".region-push-section-wrapper .municipality-ul .municipality-li:first-child").removeClass("confirmtwo")
+			}
+			else{
+				$(".region-push-section-wrapper .municipality-ul").css("display","block")
+				$(".region-push-section-wrapper .municipality-ul .municipality-li:first-child").addClass("confirmtwo")
+			}
+		})
+	
+		$(".region-push-section-wrapper .municipality-ul .municipality-li").tap(function(){
+			var index=$(this).index()
+			var selectclick=$(".region-push-section-wrapper .district-ul").eq(index-1).find(".district-li")
+			selectclick.removeClass("confirmthree")
+			$(".region-push-section-wrapper .district-ul").eq(index-1).css("display","block").siblings().css("display","none")
+			$(".region-push-section-wrapper .municipality-ul .municipality-li").eq(index).addClass("confirmtwo").siblings().removeClass("confirmtwo")
+			$(".region-push-section-wrapper .district-ul").find(".district-li").removeClass("confirmthree")
+			selectclick.tap(function(){
+				var ind=$(this).index()
+				selectclick.eq(ind).addClass("confirmthree")
+				if(selectclick.eq(0).hasClass("confirmthree")){
+					$(this).siblings().removeClass("confirmthree")
+				}
+				else{
+					selectclick.eq(ind).addClass("confirmthree")
+				}
+			})
+		})
+	
+		$(".region-push-footer .determine").tap(function(){
+			$(".map-house-wrapper .section-map-list .map-list-btn .regionli .num").css("opacity","1")
+			var len=$(".confirmthree").length
+			$(".map-house-wrapper .section-map-list .map-list-btn .regionli .num span").text(len)
+			if($(".map-house-wrapper .section-map-list .map-list-btn .regionli .num span").text()=="0"){
+				$(".map-house-wrapper .section-map-list .map-list-btn .regionli .num").css("opacity","0")
+			}
+		})
+	
+		$(".region-push-footer .empty").tap(function(){
+			$(".map-house-wrapper .screen-push .select").removeClass("selected")
+			$(".region-push-section-wrapper .municipality-ul .municipality-li").removeClass("confirmtwo")
+			$(".region-push-section-wrapper .district-ul .district-li").removeClass("confirmthree")
+			
+		})
+	
+	//筛选滚动监听事件
+	var screenS = new IScroll('.screen-push-section', {
+		scrollbars: false
+	})
+	
+	
 })
