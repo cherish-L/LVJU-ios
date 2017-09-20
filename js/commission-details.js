61a691e9f5969802024d2805b0a920ebb887240c
ljc-develop <15632877402@163.com>
2017-09-20 11:08:51 +0800

    1234567

--- js/commission-details.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	var commission_detailsS = new IScroll('.commission-details-section', {
-		scrollbars: true,
-		fadeScrollbars: true,
-		shrinkScrollbars: 'clip'
-	})
-	
-	
-})
--- js/electron-map.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	var electron_mapS = new IScroll('.electron-map-section', {
-		scrollbars: true,
-		fadeScrollbars: true,
-		shrinkScrollbars: 'clip'
-	})
-	
-	
-})
