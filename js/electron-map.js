3cc217a4a8edb2b84b01e9ec5001fe5832a8e863
ljc-develop <15632877402@163.com>
2017-09-20 10:49:30 +0800

    12345

---
+++ js/electron-map.js
@@ -0,0 +1,9 @@
+$(function(){
+	var electron_mapS = new IScroll('.electron-map-section', {
+		scrollbars: true,
+		fadeScrollbars: true,
+		shrinkScrollbars: 'clip'
+	})
+	
+	
+})
---
+++ js/property-list.js
@@ -0,0 +1,10 @@
+$(function(){
+	
+	var property_listS = new IScroll('.property-list-section', {
+		scrollbars: true,
+		fadeScrollbars: true,
+		shrinkScrollbars: 'clip'
+	})
+	
+	
+})
