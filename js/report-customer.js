2c2f9b47aa3d8946a09ac6becf2176df6135aa4f
ljc-develop <15632877402@163.com>
2017-09-23 12:44:24 +0800

    123456

--- js/report-customer.js
+++ js/report-customer.js
@@ -26,10 +26,24 @@ $(function(){
 		$(".property-info .tags-box").height("0")
 	}
 	
+	$(".property-info .tags-box .tags-box-li .delete").tap(function(){
+		$(this).parent().remove()
+		var tags_box_w_new=$(".property-info .tags-box .tags-box-ul li").width()
+		var tags_box_w_last_new=$(".property-info .tags-box .tags-box-ul li:last-child").width()
+		var tags_box_len_new=$(".property-info .tags-box .tags-box-ul li").length
+		$(".property-info .tags-box .tags-box-ul").width((tags_box_len_new-1)*tags_box_w_new+tags_box_w_last_new+2)
+		if(tags_box_len_new<=0){
+			$(".property-info .tags-box").height("0")
+		}
+		tags_boxS.refresh()
+		report_customerS.refresh()
+	})
+	
 	var tags_boxS = new IScroll('.property-info .tags-box', {
 		scrollbars: false,
 		scrollX: true,
 		scrollY: false
 	})
+	
 })
 	
