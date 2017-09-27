8cb51abed692b24f4edda80e8aa08f4a36270dc4
ljc-develop <15632877402@163.com>
2017-09-27 16:31:20 +0800

    1234567

--- js/Home-page.js
+++ js/Home-page.js
@@ -759,10 +759,10 @@ $(function() {
 						clearInterval(waiting_time)
 						$(".hot-search .hot-search-title .title-load").removeClass("refresh")
 						$(".search-page-section .search-section-wrapper").addClass("switch").siblings(".page").removeClass("switch")
+						search_pageS.refresh()
+						search_listS.refresh()
+						search_stateS.refresh()
 					}, 4000)
-					search_pageS.refresh()
-					search_listS.refresh()
-					search_stateS.refresh()
 				}
 				$(".search-page .history-search-li").tap(function() {
 					var span_txt = $(this).find("span:first-child").text()
