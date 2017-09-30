6f5cbf44be53095f7b5e08b8f9ad7dd22eb8021d
ljc-develop <15632877402@163.com>
2017-09-30 18:46:31 +0800

    12345

--- js/Home-page.js
+++ js/Home-page.js
@@ -853,7 +853,12 @@ $(function() {
 					search_listS.refresh()
 					search_stateS.refresh()
 				}, 4000)
-					
+				$("#Search-inpt").focus(function(){
+					$(".search-page-section .search-section-list").addClass("switch").siblings(".page").removeClass("switch")
+					search_pageS.refresh()
+					search_listS.refresh()
+					search_stateS.refresh()
+				})
 				$(".search-page .title-Trash").tap(function() {
 					$("#Search-inpt").blur()
 					$(".search-page .title-Trash").css("display", "none")
